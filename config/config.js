6ea20e42ad9dd4cb8b0c8b8ab22529f8b35e88b6
Oleksandr Zaytsev <oleksandr.zaytsev@trivago.com>
2018-10-26 11:43:23 +0200

    base styles

--- config/config.js
+++ config/config.js
@@ -1,10 +1,10 @@
 import { configure } from '@storybook/react';
-import '../styles/styles.css';
+import '../src/styles/styles.css';
 
 // automatically import from "components" folder all files ending in *.story.js
-const req = require.context('../components', true, /.story.js$/);
+const req = require.context('../src/components', true, /.story.js$/);
 function loadStories() {
-  req.keys().forEach((filename) => req(filename));
+    req.keys().forEach(filename => req(filename));
 }
 
 configure(loadStories, module);
